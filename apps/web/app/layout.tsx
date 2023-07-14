27a885ed2a36cadf519ae472a0f7d2ca48158b8f
Steef Janssen <steef.janssen@qonto.com>
2023-07-14 13:55:22 +0200

    copied over margin helpers from ui-kit

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "ui/global-styles/variables.css"
 import "ui/global-styles/typo.css"
 import "ui/global-styles/font-system.css"
+import "ui/global-styles/margin-helpers.css"
 import "ui/global-styles/colors/base-palette/token.css"
 import "ui/global-styles/colors/light-theme/token.css"
 
