ce400909b4648b5c2792f4dfd6d716e09dd7ae97
Baptiste Doucerain <doucerain.b@gmail.com>
2023-07-28 09:25:04 +0200

    feat(ui): add basic field component

---
+++ packages/ui/text-field/index.tsx
@@ -0,0 +1,44 @@
+import { InputHTMLAttributes, ReactNode, useState } from "react";
+import { Label } from "../label";
+import styles from "./styles.module.css";
+import cx from "classnames";
+
+/* enforcing mandatory name and id here */
+type TextFieldProps = {
+  id: string;
+  name: string; 
+  label: ReactNode;
+  errorMessage?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const TextField = ({
+  label,
+  errorMessage,
+  id,
+  required,
+  className,
+  type = "text",
+  ...props
+}: TextFieldProps) => {
+  return (
+    <div className={className}>
+      <Label htmlFor={id} className="mb-4" isOptional={!required}>
+        {label}
+      </Label>
+      <input
+        id={id}
+        aria-invalid={!!errorMessage}
+        aria-errormessage={`${id}-error`}
+        required={required}
+        type={type}
+        className={cx(styles.input, {
+          [styles["input--error"]]: !!errorMessage,
+        })}
+        {...props}
+      />
+      <span id={`${id}-error`} className={cx(styles["error-message"], 'caption')}>
+        {errorMessage}
+      </span>
+    </div>
+  );
+};
