2bb4d4e03f88b8b5fb8f600bb69aa07cd82e2bf4
Baptiste Doucerain <doucerain.b@gmail.com>
2023-08-15 13:02:13 +0200

    feat(ui): add checkbox component

---
+++ packages/ui/checkbox/index.tsx
@@ -0,0 +1,30 @@
+import { InputHTMLAttributes, useEffect, useRef } from "react";
+import cx from "classnames";
+import styles from "./styles.module.css";
+
+type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
+  indeterminate?: boolean;
+  type?: never;
+};
+
+export function Checkbox({
+  indeterminate = false,
+  className,
+  ...props
+}: CheckboxProps) {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.indeterminate = indeterminate;
+    }
+  }, [ref, indeterminate]);
+
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={cx(styles.checkbox, "overlay", className)}
+      {...props}
+    />
+  );
+}
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,3 +2,4 @@
 export * from "./button";
 export * from "./text-field";
 export * from "./status-tag";
+export * from "./checkbox";
