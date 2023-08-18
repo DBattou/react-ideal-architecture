659228d1efc3ef28988a8d1604ae8888f0ed8ffb
Baptiste Doucerain <doucerain.b@gmail.com>
2023-08-18 14:43:58 +0200

    add custom table/cell component

---
+++ apps/qonto-js/components/table/cell.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef } from "react";
+import cx from "classnames";
+import styles from "./cell.module.css";
+
+type CellProps = ComponentPropsWithoutRef<"td">;
+
+export function Cell({ className, children, ...props }: CellProps) {
+  return (
+    <td className={cx(styles.cell, className)} {...props}>
+      <div className={styles.cellContent}>{children}</div>
+    </td>
+  );
+}
