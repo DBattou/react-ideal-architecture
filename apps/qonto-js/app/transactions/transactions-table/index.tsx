373f89071548bfe4cf0faefc41609f664cc1fd83
Baptiste Doucerain <doucerain.b@gmail.com>
2023-08-18 12:45:19 +0200

    extract transactions table into its own component

---
+++ apps/qonto-js/app/transactions/transactions-table/index.tsx
@@ -0,0 +1,12 @@
+export function TransactionsTable() {
+  return (
+    <table>
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td>hi</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
