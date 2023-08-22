3e56fb4671afc34152d24268fa6a1bc651d951d5
Baptiste Doucerain <doucerain.b@gmail.com>
2023-08-22 15:02:24 +0200

    update searchParams on filters change

--- apps/qonto-js/app/transactions/filters/index.tsx
+++ apps/qonto-js/app/transactions/filters/index.tsx
@@ -1,12 +1,28 @@
 "use client";
 
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 import { SearchInput } from "./search-input";
 import styles from "./styles.module.css";
 
 export function Filters() {
+  const params = useSearchParams();
+
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleQueryChange = useDebouncedCallback((query) => {
+    const search = new URLSearchParams({ query });
+    router.replace(`${pathname}?${search}`);
+  }, 100);
+
   return (
     <div className={styles.filters}>
-      <SearchInput placeholder="Search transactions..." />
+      <SearchInput
+        placeholder="Search transactions..."
+        defaultValue={params.get("query") ?? ""}
+        onChange={handleQueryChange}
+      />
     </div>
   );
 }
