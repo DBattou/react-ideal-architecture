71ce5748cf592e40b44f9078069306f978599ebb
Baptiste Doucerain <doucerain.b@gmail.com>
2023-08-22 14:59:45 +0200

    add styles to filters

--- apps/qonto-js/app/transactions/filters/index.tsx
+++ apps/qonto-js/app/transactions/filters/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { SearchInput } from "./search-input";
+import styles from "./styles.module.css";
 
 export function Filters() {
   return (
-    <form>
+    <div className={styles.filters}>
       <SearchInput placeholder="Search transactions..." />
-    </form>
+    </div>
   );
 }
