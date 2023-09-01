2020cc843ef8a7710539c970c426faa71b1af4b3
Baptiste Doucerain <doucerain.b@gmail.com>
2023-09-01 12:37:45 +0200

    use table elements to build transactions-table

--- apps/qonto-js/app/transactions/transactions-table/index.tsx
+++ apps/qonto-js/app/transactions/transactions-table/index.tsx
@@ -1,12 +1,41 @@
+import { Cell, HeaderCell, Row, Table } from "@/components/table";
+import { Checkbox } from "ui";
+
 export function TransactionsTable() {
   return (
-    <table>
-      <thead></thead>
-      <tbody>
+    <Table caption="List of transactions">
+      <thead>
         <tr>
-          <td>hi</td>
+          <HeaderCell isSortable={false}>
+            <Checkbox />
+          </HeaderCell>
+          <HeaderCell isSortable={true} isSorted={false}>
+            Transaction
+          </HeaderCell>
+          <HeaderCell isSortable={true} isSorted={false}>
+            Method
+          </HeaderCell>
+          <HeaderCell isSortable={true} isSorted="desc">
+            Operation Date
+          </HeaderCell>
+          <HeaderCell isSortable={false}>Attachment</HeaderCell>
+          <HeaderCell isSortable={true} isSorted={false}>
+            Amount
+          </HeaderCell>
         </tr>
+      </thead>
+      <tbody>
+        <Row>
+          <Cell>
+            <Checkbox />
+          </Cell>
+          <Cell>hi</Cell>
+          <Cell>hi</Cell>
+          <Cell>hi</Cell>
+          <Cell>hi</Cell>
+          <Cell>hi</Cell>
+        </Row>
       </tbody>
-    </table>
+    </Table>
   );
 }
--- packages/ui/checkbox/index.tsx
+++ packages/ui/checkbox/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { InputHTMLAttributes, useEffect, useRef } from "react";
 import cx from "classnames";
 import styles from "./styles.module.css";
