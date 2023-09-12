c53001169c8b8a4e025bb4d55db0ca8bbfcff209
Baptiste Doucerain <doucerain.b@gmail.com>
2023-09-12 19:11:19 +0200

    add basic transaction factory

---
+++ packages/qonto-mirage/src/api/v6/transaction/factory.ts
@@ -0,0 +1,35 @@
+import { Factory } from "miragejs";
+import { Transaction } from "./model";
+import { faker } from "@faker-js/faker";
+
+export default Factory.extend<Partial<Transaction>>({
+  activityTag: () =>
+    faker.helpers.arrayElement<Transaction["activityTag"]>([
+      "other_expense",
+      "treasury_and_interco",
+      "fees",
+      "other_income",
+    ]),
+  amount: () => faker.number.float({ precision: 0.01 }),
+  counterpartyName: () => faker.company.name(),
+  emittedAt: () => faker.date.past(),
+  operationMethod: () =>
+    faker.helpers.arrayElement<Transaction["operationMethod"]>([
+      "transfer",
+      "direct_debit",
+      "card",
+      "cheque",
+      "biller",
+      "tax",
+      "pagopa_payment",
+    ]),
+  side: () =>
+    faker.helpers.arrayElement<Transaction["side"]>(["credit", "debit"]),
+  status: () =>
+    faker.helpers.arrayElement<Transaction["status"]>([
+      "completed",
+      "declined",
+      "pending",
+      "reversed",
+    ]),
+});
--- packages/qonto-mirage/src/factories.ts
+++ packages/qonto-mirage/src/factories.ts
@@ -1,3 +1,4 @@
 import user from "./auth/v2/user/factory";
+import transaction from "./api/v6/transaction/factory";
 
-export default { user };
+export default { user, transaction };
