601fad306f41afd12702428754a698ea093bef72
Steef Janssen <steef.janssen@qonto.com>
2023-09-12 11:22:00 +0200

    create makeServer on the module level vs inside the app component

--- apps/auth/pages/_app.tsx
+++ apps/auth/pages/_app.tsx
@@ -2,9 +2,10 @@ import type { AppProps } from 'next/app'
 import styles from "./layout.module.css"
 import "../styles/global.css"
 import { makeServer } from "qonto-mirage";
- 
+
+makeServer({ environment: "development" });
+
 export default function App({ Component, pageProps }: AppProps) {
-  makeServer({ environment: "development" });
   const AnyComponent = Component as any;
   return (
     <div className={styles["l-container__app"] }>
