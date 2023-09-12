194da443b5aaeefc77ccb2b6f4ed8fd149b0e249
Steef Janssen <steef.janssen@qonto.com>
2023-09-12 08:33:48 +0200

    import missing mirageServer correctly again

--- apps/auth/pages/_app.tsx
+++ apps/auth/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import styles from "./layout.module.css"
 import "../styles/global.css"
+import MirageServer from '@/components/mirage-server'
  
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={styles["l-container__app"] }>
+      <MirageServer />
       <Component {...pageProps} />
     </div>
   )
