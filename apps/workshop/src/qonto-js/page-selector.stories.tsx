f4ea00bff09cf3a3e6a6ca172d7f1b88a25a7577
Baptiste Doucerain <doucerain.b@gmail.com>
2023-09-25 09:43:31 +0200

    add qonto-js/page-selector story in workshop

---
+++ apps/workshop/src/qonto-js/page-selector.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PageSelector } from "qonto-js/components/page-selector";
+
+const meta = {
+  title: "qonto-js/page-selector",
+  component: PageSelector,
+  tags: ["autodocs"],
+  argTypes: {
+    onPageChange: { action: "page selected" },
+    onPerPageChange: { action: "per_page selected" }
+  }
+} satisfies Meta<typeof PageSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = { args: {
+    page: 1,
+    perPage: 25,
+    totalCount: 100
+} };
