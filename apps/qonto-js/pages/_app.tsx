126c583f40ec6374f985cef2464bd38e50763b15
Wandrille VERLUT <wandrille.verlut.ext@qonto.com>
2023-09-28 11:50:52 +0200

    add tanstack

--- apps/qonto-js/pages/_app.tsx
+++ apps/qonto-js/pages/_app.tsx
@@ -1,9 +1,26 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import { makeServer } from "qonto-mirage";
 import "@/styles/global.css";
 
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 makeServer({ environment: "development" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
