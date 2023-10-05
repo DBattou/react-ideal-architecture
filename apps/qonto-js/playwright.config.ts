2ff6f9ed157761dd67c58d6c56159e0baecc0a1f
nickschot <nick.schot.ext@qonto.com>
2023-10-05 11:11:44 +0200

    Contain all playwright artifacts/reports in .playwright folder, add to gitignore

--- apps/qonto-js/playwright.config.ts
+++ apps/qonto-js/playwright.config.ts
@@ -5,8 +5,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests/",
+  outputDir: "./.playwright/test-results/",
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
-  snapshotDir: "./__snapshots__",
+  snapshotDir: "./.playwright/__snapshots__",
   /* Maximum time one test can run for. */
   timeout: 10 * 1000,
   /* Run tests in files in parallel */
@@ -18,7 +19,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["junit", { outputFile: 'test-results-junit.xml' }]] : "html",
+  reporter: process.env.CI
+      ? [["junit", { outputFile: './.playwright/report/junit.xml' }]]
+      : [["html", { outputFolder: './.playwright/report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
