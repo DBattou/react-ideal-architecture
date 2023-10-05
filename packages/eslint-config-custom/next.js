800e7272b09ff784b9f28f1c778ebf3ac27e5135
Baptiste Doucerain <doucerain.b@gmail.com>
2023-10-05 11:36:55 +0200

    eslint next: prefer types over interfaces, disable next/image errors for now

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -40,5 +40,7 @@ module.exports = {
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@next/next/no-img-element": "off",
   },
 };
