0af784dcbfaf26cacc261d198e23870f95bf75cb
Ali Soylucicek <ali.soylucicek@qonto.com>
2023-10-11 12:22:11 +0200

    Remove old transactions table

--- apps/qonto-js/src/components/transactions-table/columns/operation-method.tsx
+++
@@ -1,7 +0,0 @@
-import { columnHelper } from "./utils/helper";
-
-export const operationMethodColumn = columnHelper.accessor("operationMethod", {
-  id: "operation_method",
-  header: "Method",
-  cell: (info) => info.getValue(),
-});
--- apps/qonto-js/src/components/transactions-table/columns/utils/helper.ts
+++
@@ -1,4 +0,0 @@
-import { createColumnHelper } from "@tanstack/react-table";
-import type { Transaction } from "@/services/transactions";
-
-export const columnHelper = createColumnHelper<Transaction>();
