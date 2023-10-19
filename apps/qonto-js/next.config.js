8ce4951ca08e2633135ef419d8fdfab0f8135997
Jerome Smadja <jerome.smadja.ext@qonto.com>
2023-10-19 10:23:43 +0200

    Added Dockerfile

--- apps/qonto-js/next.config.js
+++ apps/qonto-js/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  // For docker:  https://github.com/vercel/next.js/tree/canary/examples/with-docker#in-existing-projects
+  // output: 'standalone',
 };
 
 module.exports = nextConfig;
