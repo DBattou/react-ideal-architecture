7f19e99bd58ccb167cb92a89b57c6f308918a604
Ali Soylucicek <ali.soylucicek@qonto.com>
2023-10-27 12:03:38 +0200

    update qonto-mirage package with types

--- packages/qonto-mirage/src/api/v6/transaction/model.ts
+++ packages/qonto-mirage/src/api/v6/transaction/model.ts
@@ -1,4 +1,4 @@
 import { Model } from "miragejs";
-import type { Transaction } from "qonto-api";
+import type { Transaction } from "transactions-entity";
 
 export default Model.extend<Partial<Transaction>>({});
