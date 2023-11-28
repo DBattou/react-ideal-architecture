99f82fa143a4f97d5a795ba88b7a0387d2efb6f8
Baptiste Doucerain <doucerain.b@gmail.com>
2023-11-28 11:41:41 +0100

    disable routing mock for filters component

--- apps/qonto-js/src/components/filters/index.spec.tsx
+++ apps/qonto-js/src/components/filters/index.spec.tsx
@@ -1,12 +1,9 @@
 import { test, expect } from "@playwright/experimental-ct-react";
-import type { HooksConfig } from "../../../playwright";
 import { Filters } from ".";
 
 test.describe(() => {
   test("displays a search bar", async ({ mount, page }) => {
-    await mount<HooksConfig>(<Filters />, {
-      hooksConfig: { enableRouting: true },
-    });
+    await mount(<Filters />);
     await expect(
       page.getByPlaceholder("Search transactions...")
     ).toBeAttached();
