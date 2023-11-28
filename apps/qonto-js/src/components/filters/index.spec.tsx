50b546df66e3a4dc2936b61c500c13814da62c41
Baptiste Doucerain <doucerain.b@gmail.com>
2023-11-28 11:59:09 +0100

    add tests for new filters props

--- apps/qonto-js/src/components/filters/index.spec.tsx
+++ apps/qonto-js/src/components/filters/index.spec.tsx
@@ -1,11 +1,36 @@
+import sinon from "sinon";
 import { test, expect } from "@playwright/experimental-ct-react";
 import { Filters } from ".";
 
-test.describe(() => {
+test.describe("query input", () => {
   test("displays a search bar", async ({ mount, page }) => {
     await mount(<Filters />);
     await expect(
       page.getByPlaceholder("Search transactions...")
     ).toBeAttached();
+  });
+
+  test("should use initialQuery to prefill the input value", async ({
+    mount,
+    page,
+  }) => {
+    await mount(<Filters initialQuery="wassup" />);
+
+    await expect(page.getByPlaceholder("Search transactions...")).toHaveValue(
+      "wassup"
+    );
+  });
+
+  test("should call onQueryChange when typing in the search bar", async ({
+    mount,
+    page,
+  }) => {
+    const queryChangeHandler = sinon.spy();
+
+    await mount(<Filters onQueryChange={queryChangeHandler} />);
+
+    await page.getByPlaceholder("Search transactions...").fill("Yay");
+
+    expect(queryChangeHandler.calledOnceWith("Yay")).toBeTruthy();
   });
 });
