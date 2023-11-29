69580aeac87b59787b835790fab756b3127bda50
Steef Janssen <steef.janssen@qonto.com>
2023-11-29 09:21:07 +0100

    remove redundant check for router to be ready
    this is not needed anymore with the new app router

--- apps/qonto-js/src/app/transactions/page.tsx
+++ apps/qonto-js/src/app/transactions/page.tsx
@@ -68,7 +68,6 @@ export default function TransactionsIndex(): JSX.Element {
       ),
     placeholderData: PLACEHOLDER_DATA,
     keepPreviousData: true,
-    enabled: router.isReady,
   });
 
   const handleSortingChange = (
