c9f82db4a26878e364d6a25bdfd0c5e6e9959357
Baptiste Doucerain <doucerain.b@gmail.com>
2023-11-29 13:17:11 +0100

    fix mock handler return type to use serialized transactions payload

--- apps/qonto-js/src/mocks/handlers.ts
+++ apps/qonto-js/src/mocks/handlers.ts
@@ -1,9 +1,12 @@
 import { rest } from "msw";
 import type { TransactionsListPayload } from "@/services/transactions";
+import type { Serialized } from "@/types/utils";
 import { transactionsPlayload } from "./fixtures/transactions";
 
 export default [
   rest.post("/api/v6/transactions/search", (_req, res, ctx) => {
-    return res(ctx.json<TransactionsListPayload>(transactionsPlayload));
+    return res(
+      ctx.json<Serialized<TransactionsListPayload>>(transactionsPlayload)
+    );
   }),
 ];
