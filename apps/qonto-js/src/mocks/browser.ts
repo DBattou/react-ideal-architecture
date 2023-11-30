0f4402e244dc4db5a2c8877376f7de84d8b5fc66
Steef Janssen <steef.janssen@qonto.com>
2023-11-30 15:10:00 +0100

    move starting of msw worker where its setup
    this allows types to be more correctly inferred

--- apps/qonto-js/src/mocks/browser.ts
+++ apps/qonto-js/src/mocks/browser.ts
@@ -1,4 +1,15 @@
 import { setupWorker } from "msw";
 import handlers from "./handlers";
 
-export const worker = setupWorker(...handlers);
+const worker = setupWorker(...handlers);
+
+void worker.start({
+  onUnhandledRequest(request, print) {
+    // Ignore any requests relatec to Next.js' static file serving.
+    if (request.url.pathname.includes("/_next/")) {
+      return;
+    }
+    // Otherwise, print an unhandled request warning.
+    print.warning();
+  },
+});
--- apps/qonto-js/src/providers/msw.tsx
+++ apps/qonto-js/src/providers/msw.tsx
@@ -4,22 +4,7 @@ import { useEffect } from "react";
 export function MSWComponent(): null {
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-      const { worker } = require("@/mocks/browser");
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-      worker.start({
-        onUnhandledRequest(request, print) {
-          // Ignore any requests relatec to Next.js' static file serving.
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-          if (request.url.includes("/_next/")) {
-            return;
-          }
-
-          // Otherwise, print an unhandled request warning.
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-          print.warning();
-        },
-      });
+      require("@/mocks/browser");
     }
   }, []);
 
