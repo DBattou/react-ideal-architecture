fbce943cf15d4d31d7892447011c94f4836cd9a6
Steef Janssen <steef.janssen@qonto.com>
2023-11-30 11:06:48 +0100

    filter out next.js static requests from being logged by msw

--- apps/qonto-js/src/providers/msw.tsx
+++ apps/qonto-js/src/providers/msw.tsx
@@ -7,7 +7,19 @@ export function MSWComponent(): null {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
       const { worker } = require("@/mocks/browser");
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-      worker.start();
+      worker.start({
+        onUnhandledRequest(request, print) {
+          // Ignore any requests relatec to Next.js' static file serving.
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          if (request.url.includes("/_next/")) {
+            return;
+          }
+
+          // Otherwise, print an unhandled request warning.
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          print.warning();
+        },
+      });
     }
   }, []);
 
