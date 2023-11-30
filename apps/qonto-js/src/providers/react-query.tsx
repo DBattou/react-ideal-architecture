2b13aea158bf3fc91343210aca9256b6cb2dad1c
Baptiste Doucerain <doucerain.b@gmail.com>
2023-11-30 09:19:40 +0100

    disable RQ queries retries in test mode

--- apps/qonto-js/src/providers/react-query.tsx
+++ apps/qonto-js/src/providers/react-query.tsx
@@ -7,7 +7,15 @@ export function ReactQueryProvider({
   children,
 }: React.PropsWithChildren): JSX.Element {
   // eslint-disable-next-line react/hook-use-state
-  const [client] = useState(new QueryClient());
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: process.env.NEXT_PUBLIC_ENV === "test" ? false : 3,
+        },
+      },
+    })
+  );
 
   return (
     <QueryClientProvider client={client}>
