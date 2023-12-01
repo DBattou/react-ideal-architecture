93fdb8ea8a961cc91c1ebd679d08385b5cbb41e0
Steef Janssen <steef.janssen@qonto.com>
2023-12-01 11:30:57 +0100

    enable qonto-mirage dev server in qonto-js
    this will enable it to use msw from mirage

--- apps/qonto-js/src/mocks/browser.ts
+++ apps/qonto-js/src/mocks/browser.ts
@@ -1,15 +1,3 @@
-import { setupWorker } from "msw";
-import handlers from "./handlers";
+import { makeServer } from "qonto-mirage";
 
-const worker = setupWorker(...handlers);
-
-void worker.start({
-  onUnhandledRequest(request, print) {
-    // Ignore any requests relatec to Next.js' static file serving.
-    if (request.url.pathname.includes("/_next/")) {
-      return;
-    }
-    // Otherwise, print an unhandled request warning.
-    print.warning();
-  },
-});
+makeServer({ environment: "development" });
