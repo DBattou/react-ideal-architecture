fae40e18a73a0aca34c25490f250e3d96f500bcc
Baptiste Doucerain <doucerain.b@gmail.com>
2023-12-07 13:29:27 +0100

    improve transactions sidebar styles to match qonto-js

--- apps/qonto-js/src/app/transactions/[id]/page.tsx
+++ apps/qonto-js/src/app/transactions/[id]/page.tsx
@@ -1,3 +1,13 @@
-export default function TransactionsShow(): JSX.Element {
-  return <div>I'm a transaction</div>;
+import styles from "./styles.module.css";
+
+type PageProps = {
+  params: { id: string };
+};
+
+export default function TransactionsShow({ params }: PageProps): JSX.Element {
+  return (
+    <section className={styles.panel}>
+      Details for transaction {params.id}
+    </section>
+  );
 }
